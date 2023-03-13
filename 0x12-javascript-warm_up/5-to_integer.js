59dd639f4d8b0ba5019ae73f4bcf7356d0c21791
Mayoralty24 <gbengamayowadavid@gmail.com>
2023-03-13 06:52:53 -0700

    java

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,28 +1,7 @@
 #!/usr/bin/node
-/*
-  A JS script that prints My number: <first argument converted in integer>
-  of the process.argv array.
-  @value: A variable holding the value of the the process.argv at index
-          2. then using the Number constructor to change the data type.
-  This can be archived using isNaN() method or typeof() However isNaN is
-  more suitable for this case.
-  Handling float to int i used the bitwise Or operator However parseint()
-  works and also the Math.trunc() works.
-*/
 
-const value = Number(process.argv[2]);
-/* method 1
-if (isNaN(value)) {
-  console.log('Not a number');
-} else {
-  console.log('My number: ', value | 0);
-}
-*/
+const { argv } = require('process');
+const num = parseInt(argv[2]);
 
-// Method 2
+console.log(Number.isInteger(num) ? `My number: ${num}` : 'Not a number');
 
-if (isNaN(value)) {
-  console.log('Not a number');
-} else {
-  console.log('My number: ', Math.trunc(value));
-}
